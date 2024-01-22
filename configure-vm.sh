a50eb12f20bc395c3e062084398e65b885dd5493
4TL4S <root@atlas-sec.de>
2024-01-22 12:02:02 +0100

    test fix

--- configure-vm.sh
+++ configure-vm.sh
@@ -12,8 +12,6 @@ strip_color_codes() {
 }
 
 # Redirect stdout and stderr to the terminal and log file, with color codes stripped
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
 exec > >(strip_color_codes | tee -a "$LOG_FILE") 2>&1
 
 
