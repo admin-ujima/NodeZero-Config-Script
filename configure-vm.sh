75d9be7e1622c482d4a50f247190e49dbf19bf6a
Cameron Boyd <cameron.boyd@ujima.de>
2025-02-10 15:43:49 +0100

    added cleanup for xtna install

--- configure-vm.sh
+++ configure-vm.sh
@@ -50,7 +50,7 @@ check_xplicittrust() {
     sudo apt update
     sudo apt --yes install wireguard wireguard-tools wget iptables ipset
     wget https://dl.xplicittrust.com/xtna-agent_amd64.deb
-    sudo dpkg -i ~/Downloads/xtna-agent_amd64.deb
+    sudo dpkg -i xtna-agent_amd64.deb
     rm -rf xtna-agent_amd64.deb
   else
     echo -e "${GREEN}[DONE] - xplicittrust is already installed.${NC}"
