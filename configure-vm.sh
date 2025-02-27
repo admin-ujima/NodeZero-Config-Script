5e75a340ccaa5ed8d3bc6744878865fa5444f224
Cameron Boyd <cameron.boyd@ujima.de>
2025-02-27 12:29:57 +0100

    test change

--- configure-vm.sh
+++ configure-vm.sh
@@ -8,7 +8,7 @@ LOG_FILE=".logs/script_log_$(date +'%d.%m.%Y-%H:%M:%S').log"
 
 # Function to strip ANSI color codes using unbuffered sed
 strip_color_codes() {
-  sed -ru "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+    sed -ru "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
 }
 
 # Redirect stdout and stderr to Logfile
@@ -35,44 +35,44 @@ TOKEN="M1ID9Z47kRlbCvtgEHbY2kszJ7C0CiYkXqqgjLeNw3k"
 
 # Function to display script usage
 usage() {
-  echo -e "${BLUE}Usage: $0 [OPTIONS]"
-  echo -e "Options:"
-  echo -e "  -s, --sid SID_NAME  Set the SID (e.g., KTH, UJ)"
-  echo -e "  -d, --domain DOMAIN This is the Domain used for xplicittrust (e.g. ujima.de)"
-  echo -e "  -i, --index INDEX   This is the index which is part of the machine hostname"
-  echo -e "  -f, --file PATH     This way you can not use the interactive console and instead load a config"
-  echo -e "  -h, --help          Display this help message${NC}"
-  exit 1
+echo -e "${BLUE}Usage: $0 [OPTIONS]"
+echo -e "Options:"
+echo -e "  -s, --sid SID_NAME  Set the SID (e.g., KTH, UJ)"
+echo -e "  -d, --domain DOMAIN This is the Domain used for xplicittrust (e.g. ujima.de)"
+echo -e "  -i, --index INDEX   This is the index which is part of the machine hostname"
+echo -e "  -f, --file PATH     This way you can not use the interactive console and instead load a config"
+echo -e "  -h, --help          Display this help message${NC}"
+exit 1
 }
 
 # Function to check if xplicittrust is installed
 check_xplicittrust() {
-  if ! command -v xtna-agent &> /dev/null; then
-    # Install xplicittrust
-    echo -e "${MAGENTA}[INFO] - xplicittrust is missing! Installing it now...${NC}"
-    sudo apt update
-    sudo apt --yes install wireguard wireguard-tools wget iptables ipset
-    wget https://dl.xplicittrust.com/xtna-agent_amd64.deb
-    sudo dpkg -i xtna-agent_amd64.deb
-    rm -rf xtna-agent_amd64.deb
-  else
-    echo -e "${GREEN}[DONE] - xplicittrust is already installed.${NC}"
-  fi
+if ! command -v xtna-agent &> /dev/null; then
+# Install xplicittrust
+echo -e "${MAGENTA}[INFO] - xplicittrust is missing! Installing it now...${NC}"
+sudo apt update
+sudo apt --yes install wireguard wireguard-tools wget iptables ipset
+wget https://dl.xplicittrust.com/xtna-agent_amd64.deb
+sudo dpkg -i xtna-agent_amd64.deb
+rm -rf xtna-agent_amd64.deb
+else
+echo -e "${GREEN}[DONE] - xplicittrust is already installed.${NC}"
+fi
 }
 
 # Function to handle configuration from environment variables or configuration file
 handle_config() {
-  if [ -z "$CONFIG_FILE" ]; then
-    echo -e "${RED}[ERROR] - The File Path is empty. This is mandatory for this mode!${NC}"
-    exit 1
-  fi
-  
-  if [ -f "$CONFIG_FILE" ]; then
-    source "$CONFIG_FILE"
-  else
-    echo -e "${RED}[ERROR] - Config file not found: $CONFIG_FILE${NC}"
-    exit 1
-  fi
+if [ -z "$CONFIG_FILE" ]; then
+echo -e "${RED}[ERROR] - The File Path is empty. This is mandatory for this mode!${NC}"
+exit 1
+fi
+
+if [ -f "$CONFIG_FILE" ]; then
+source "$CONFIG_FILE"
+else
+echo -e "${RED}[ERROR] - Config file not found: $CONFIG_FILE${NC}"
+exit 1
+fi
 }
 
 
@@ -133,94 +133,95 @@ handle_config() {
 # }
 
 setup_h3_runner() {
-  # Setup required env variables
-  #setup_h3_path
+# Setup required env variables
+#setup_h3_path
 
-  # Check if Nodezero API Key exists else handle steps
-  #setup_h3_authentication
+# Check if Nodezero API Key exists else handle steps
+#setup_h3_authentication
 
-  echo -e "${YELLOW}[INFO] - Starting runner checkup...${NC}"
+echo -e "${YELLOW}[INFO] - Starting runner checkup...${NC}"
 
-  # Check if H3 runner is already set up
-  runner_name=$(/home/nodezero/h3-cli/bin/h3 runners | jq --raw-output .name 2>/dev/null)
-  code=$?
+# Check if H3 runner is already set up
+rm -rf /tmp/.resolve_fragments*
+runner_name=$(/home/nodezero/h3-cli/bin/h3 runners | jq --raw-output .name 2>/dev/null)
+code=$?
 
-  echo -e "${YELLOW}[DEBUG] - Runner Name: $runner_name, Code: $code${NC}"
+echo -e "${YELLOW}[DEBUG] - Runner Name: $runner_name, Code: $code${NC}"
 
-  if [ $code -eq 0 ] && [ "$runner_name" = "$MODIFIED_HOSTNAME" ]; then
-    echo -e "${RED}[DONE] - H3 runner with the name $MODIFIED_HOSTNAME is already set up. If you want a additional runner change the INDEX${NC}"
-    exit 1
-  fi
+if [ $code -eq 0 ] && [ "$runner_name" = "$MODIFIED_HOSTNAME" ]; then
+echo -e "${RED}[DONE] - H3 runner with the name $MODIFIED_HOSTNAME is already set up. If you want a additional runner change the INDEX${NC}"
+exit 1
+fi
 
-  # H3 runner is not set up, set it up with API key
-  echo -e "${MAGENTA}[INFO] - H3 runner is not set up. Setting it up with API key...${NC}"
+# H3 runner is not set up, set it up with API key
+echo -e "${MAGENTA}[INFO] - H3 runner is not set up. Setting it up with API key...${NC}"
 
-  bash /home/nodezero/h3-cli/easy_install.sh "$NODEZERO_APIKEY" "$MODIFIED_HOSTNAME"
+bash /home/nodezero/h3-cli/easy_install.sh "$NODEZERO_APIKEY" "$MODIFIED_HOSTNAME"
 
-  echo -e "${GREEN}[DONE] - H3 runner set up successfully.${NC}"
+echo -e "${GREEN}[DONE] - H3 runner set up successfully.${NC}"
 }
 
 # Function to process command line options
 process_options() {
-  while [[ "$#" -gt 0 ]]; do
-    case $1 in
-      -s|--sid)
-        SID="$2"
-        shift 2
-        ;;
-      -i|--index)
-        SID="$2"
-        shift 2
-        ;;
-      -d|--domain)
-        DOMAIN="$2"
-        shift 2
-        ;;
-      -h|--help)
-        usage
-        ;;
-      *)
-        echo -e "${RED}[ERROR] - Unknown option: $1${NC}"
-        usage
-        ;;
-    esac
-  done
+while [[ "$#" -gt 0 ]]; do
+case $1 in
+    -s|--sid)
+    SID="$2"
+    shift 2
+    ;;
+    -i|--index)
+    SID="$2"
+    shift 2
+    ;;
+    -d|--domain)
+    DOMAIN="$2"
+    shift 2
+    ;;
+    -h|--help)
+    usage
+    ;;
+    *)
+    echo -e "${RED}[ERROR] - Unknown option: $1${NC}"
+    usage
+    ;;
+esac
+done
 }
 
 # Check for sudo privileges
 if [ "$EUID" -ne 0 ]; then
-  echo -e "${RED}[ERROR] - This script needs sudo privileges. Please run with sudo.${NC}"
-  exit 1
+echo -e "${RED}[ERROR] - This script needs sudo privileges. Please run with sudo.${NC}"
+exit 1
 fi
 
 # Check if the non-interactive config file based approach want to be used
 if [ "$1" == "-f" ] || [ "$1" == "--file" ]; then
-  CONFIG_FILE="$2"
-  # Handle configuration from file directly
-  handle_config
-  # This shifts the input arguments 2 places to the left
-  shift 2
+CONFIG_FILE="$2"
+# Handle configuration from file directly
+handle_config
+# This shifts the input arguments 2 places to the left
+shift 2
 else
-  # Enter the loop to process command line options
-  process_options "$@"
+# Enter the loop to process command line options
+process_options "$@"
 fi
 
 # Check if SID is provided
 if [ -z "$SID" ]; then
-  echo -e "${RED}[ERROR] - SID is mandatory. Use the -s option to specify the SID.${NC}"
-  usage
+echo -e "${RED}[ERROR] - SID is mandatory. Use the -s option to specify the SID.${NC}"
+usage
 fi
 
 # Check if xplicittrust token is provided
 if [ -z "$INDEX" ]; then
-  echo -e "${RED}[ERROR] - INDEX is mandatory. Use the -i option to specify the Index of the Machine.${NC}"
-  usage
+echo -e "${RED}[ERROR] - INDEX is mandatory. Use the -i option to specify the Index of the Machine.${NC}"
+usage
 fi
 
 # Check if xplicittrust domain is provided
 if [ -z "$DOMAIN" ]; then
-  echo -e "${RED}[ERROR] - DOMAIN is mandatory. Use the -d option to specify the XplicitTrust Domain to register to.${NC}"
-  usage
+echo -e "${RED}[ERROR] - DOMAIN is mandatory. Use the -d option to specify the XplicitTrust Domain to register to.${NC}"
+usage
 fi
 
 # Sanitize SID and HOSTNAME from any trailing linebreaks
@@ -239,8 +240,8 @@ sudo hostnamectl set-hostname "$MODIFIED_HOSTNAME"
 hostname_res=$?
 
 if [ ! $hostname_res -eq 0 ]; then
-  echo -e "${RED}[ERROR] - Something went wrong when setting hostname!${NC}"
-  exit 1
+echo -e "${RED}[ERROR] - Something went wrong when setting hostname!${NC}"
+exit 1
 fi
 
 # Update /etc/hosts
@@ -260,10 +261,10 @@ sudo xtna-util -domain "$DOMAIN" -token "$TOKEN"
 xtna_exit_code=$?
 
 if [ $xtna_exit_code -eq 0 ];then
-  echo -e "${CYAN}[INFO] - The xplicittrust connection has been established!${NC}"
+echo -e "${CYAN}[INFO] - The xplicittrust connection has been established!${NC}"
 else
-  echo -e "${RED}[ERROR] - Something went wrong, when trying to establish connection to XplicitTrust...Probably the Token expired!${NC}"
-  exit 1
+echo -e "${RED}[ERROR] - Something went wrong, when trying to establish connection to XplicitTrust...Probably the Token expired!${NC}"
+exit 1
 fi
 
 # Reboot to apply changes
